536705edf3cbe0f6f6a3e008364923ceebb21ef2
jonathan <jonathan.ruiz.1793@gmail.com>
2024-09-09 14:31:13 -0500

    Solved Problem

--- closest.ts
+++ closest.ts
@@ -1,6 +1,22 @@
 export function closest(nums: number[], target: number): number {
-   // Implement me
-   return 0;
+  // Implement me
+  if (nums.length < 3) {
+    throw new Error("array must contain at least 3 values to sum up");
+  }
+  let closestSum = nums[0] + nums[1] + nums[2];
+
+  for (let i = 0; i < nums.length - 2; i++) {
+    for (let j = i + 1; j < nums.length - 1; j++) {
+      for (let k = j + 1; k < nums.length; k++) {
+        const currentSum = nums[i] + nums[j] + nums[k];
+        if (Math.abs(currentSum - target) < Math.abs(closestSum - target)) {
+          closestSum = currentSum;
+        }
+      }
+    }
+  }
+
+  return closestSum;
 }
 
-module.exports = { closest }
+module.exports = { closest };
